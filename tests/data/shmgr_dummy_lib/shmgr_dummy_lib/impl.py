d3f40189b19bf20a83516da3f2b4cbb0f3014311
Bryan Bugyi <bryanbugyi34@gmail.com>
2021-12-12 18:08:27 -0500

    Finish shmgr_dummy_lib/impl.py

--- tests/data/shmgr_dummy_lib/shmgr_dummy_lib/impl.py
+++ tests/data/shmgr_dummy_lib/shmgr_dummy_lib/impl.py
@@ -1,1 +1,11 @@
 """Dummy shell library plugin client implementation."""
+
+from typing import Tuple
+
+from shmgr.plugin import hookimpl
+
+
+@hookimpl
+def register_shell_libs() -> Tuple[str, str, Tuple[int, int, int]]:
+    """We register the dummy shell library using this hook implementation."""
+    return ("shmgr_dummy_lib", "dummy", (3, 0, 0))
